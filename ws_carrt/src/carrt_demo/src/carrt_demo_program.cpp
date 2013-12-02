57c62d98c79d7236990a1def5db3040c5d304557
Andoni Aguirrezabal <aaguirrezaba@mail.usf.edu>
2013-12-02 16:54:13 -0500

    Added the current work that was done with the Baxter
    Signed-off-by: Andoni Aguirrezabal <aaguirrezaba@mail.usf.edu>

---
+++ ws_carrt/src/carrt_demo/src/carrt_demo_program.cpp
@@ -0,0 +1,77 @@
+// ROS
+#include <ros/ros.h>
+
+// MoveIt!
+#include <moveit/move_group_interface/move_group.h>
+
+#include <geometry_msgs/PoseStamped.h>
+
+// Baxter Utilities
+#include <baxter_control/baxter_utilities.h>
+
+namespace carrt_demo
+{
+
+class CARRTDemoProgram
+{
+public:
+    // Our Interface with MoveIt
+    boost::scoped_ptr<move_group_interface::MoveGroup> move_group_;
+
+    // Baxter Helper
+    baxter_control::BaxterUtilities baxter_util_;
+
+    CARRTDemoProgram()
+    {
+        ros::NodeHandle nh;
+
+        // Create MoveGroup for the Right_Arm
+        move_group_.reset(new move_group_interface::MoveGroup("right_arm"));
+
+        // Let Everything Load
+        ros::Duration(1.0).sleep();
+
+        // Enable Baxter
+        if(!baxter_util_.enableBaxter())
+            return;
+
+        geometry_msgs::PoseStamped ee_pose;
+
+        while(ros::ok()){
+            ee_pose = move_group_->getCurrentPose("right_gripper_l_finger");
+            ROS_INFO_STREAM_NAMED("carrt_demo", "Postion (X, Y, Z): (" << ee_pose.pose.position.x << ", " << ee_pose.pose.position.y << ", " << ee_pose.pose.position.z << ")");
+            ros::Duration(4.0).sleep();
+        }
+
+        // Move to gravity neutral position
+        baxter_util_.positionBaxterNeutral();
+
+        // Disable servos
+        baxter_util_.disableBaxter();
+
+    }
+
+    bool StartRoutine() {
+        //Wait for User Input
+        //Tuck Baxter
+        //Navigate PowerBot to destination
+        //Untuck Baxter
+        //Laucnh OPE
+        //Navigate to OPE
+    }
+};
+
+} //namespace
+
+int main(int argc, char **argv) {
+    //Start our ROS Node with an Async Thread Spinner (1 Thread)
+    ros::init(argc, argv, "carrt_demo_program");
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    carrt_demo::CARRTDemoProgram();
+
+    ros::shutdown();
+
+    return 0;
+} //main
---
+++ ws_carrt/src/carrt_demo/src/carrt_kinect_tf.cpp
@@ -0,0 +1,32 @@
+//ROS
+#include <ros/ros.h>
+
+//ROS Transform
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "carrt_kinect_tf_broadcaster");
+    ros::NodeHandle node;
+
+    tf::TransformBroadcaster kinect_br_;
+    tf::Transform kinect_tf_;
+
+    //Set the broadcaster delay
+    ros::Rate rate(5.0);
+
+    //Loop while the node is still valid
+    while (node.ok()) {
+        tf::Matrix3x3 kinect_matrix;
+        //Rotate to 27 Degrees Down
+        kinect_matrix.setValue( 0 , -0.3987 ,  0.9171 ,
+                               -1 ,  0      ,  0      ,
+                                0 , -0.9171 , -0.3987 );
+        kinect_tf_.setOrigin( tf::Vector3(0.2, 0.0, 0.25) );
+        //kinect_tf_.setRotation( tf::createQuaternionFromRPY(0.0, 2.04203522, -1.57079633) );
+        kinect_tf_.setBasis(kinect_matrix); //Set Rotation Matrix
+        kinect_br_.sendTransform(tf::StampedTransform(kinect_tf_, ros::Time::now(), "base", "kinect_head"));
+        rate.sleep();
+    }
+
+    return 0;
+} //main
